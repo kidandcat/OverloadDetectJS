186f74cb4e5d23f403fd54265b8285f19cf9744e
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2016-05-03 12:13:25 +0200

    Create OverloadDetect.js

---
+++ OverloadDetect.js
@@ -0,0 +1,34 @@
+(function(){
+          var numbers = 0;
+          setInterval(function(){
+            var x = timer.end();
+            timer.start();
+            if(x > 1.300 || x < 0.700){
+              numbers++;
+            }
+            if(numbers > 3){
+              alert('System Overload!¡!');
+              numbers = 0;
+            }
+          },1000);
+
+          var timer = (function(){
+            var obj = {};
+            var begin = null;
+            var finish = null;
+
+            obj.start = function(){
+              begin = null;
+              finish = null;
+              begin = new Date().getTime();
+            }
+
+            obj.end = function(){
+              finish = (begin)?new Date().getTime() - begin:0;
+              var n = finish/1000;
+              //console.log(n + 's');
+              return n;
+            }
+            return obj;
+          })();
+        })()
